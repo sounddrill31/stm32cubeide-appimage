7eff2e1df320f7b28df05894c26f6d72839bd35a
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-07-21 11:41:43 +0000

    Get proper dir structure of AppDir

--- setup_stm32cube.sh
+++ setup_stm32cube.sh
@@ -14,8 +14,6 @@ echo "Output Folder: $OUTPUT"
 echo "Setting up STM32Cube environment..."
 echo "Displaying contents of the input directory: $INPUT"
 ls -lh $INPUT
-tree $INPUT
-
 
 echo "Extracting script from input directory..."
 mkdir -p prebuilts/${VERSION}/extracted
@@ -28,7 +26,8 @@ cp scripts/ci_*.sh prebuilts/${VERSION}/extracted/
 cd prebuilts/${VERSION}/extracted/
 echo "Running ci_install.sh to prepare the AppDir structure..."
 bash ci_install.sh --output-prefix AppDir
+cd ../../..
 )
 
 echo "STM32Cube environment setup complete. Showing AppDir structure:"
-tree AppDir
+sudo tree AppDir
