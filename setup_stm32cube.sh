73c27c06eaef04aaff87722694549a509f7597c0
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-07-21 10:00:40 +0000

    prepare extracting tar from cube sh

--- setup_stm32cube.sh
+++ setup_stm32cube.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/bash
 
+VERSION=$(cat current_version.txt)
+
 # Dummy script for now, just shows the files in the input directory
-echo "Preparing Script"
-echo "Input: $0"
-echo "Output Folder: $1"
+echo "Running $0 with arguments: $@"
+
+INPUT=$1
+OUTPUT=$2
+echo "Preparing Script $INPUT  ..."
+echo "Input: $INPUT"
+echo "Output Folder: $OUTPUT"
 
 echo "Setting up STM32Cube environment..."
-echo "Displaying contents of the input directory: $0"
-ls -lh $0
-tree $0
+echo "Displaying contents of the input directory: $INPUT"
+ls -lh $INPUT
+tree $INPUT
 
 
-# echo "Extracting script from input directory..."
-# echo "Extracting STM32Cube files..."
+echo "Extracting script from input directory..."
+mkdir -p prebuilts/${VERSION}/extracted
+bash $INPUT --tar xvf -C prebuilts/${VERSION}/extracted && (echo "Finished Extracting! Showing Extracted Files"; tree prebuilts/${VERSION}/extracted) # We know the script has a tarball and a self-extracting command
+
+
+# echo "Installing STM32Cube files to temporary path..."
 # echo "STM32Cube environment setup complete."
