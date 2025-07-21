b4fda940c544b40cbc86389a33cef756c3d07cbf
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-07-21 09:49:28 +0000

    Double check variables

--- setup_stm32cube.sh
+++ setup_stm32cube.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
 
 # Dummy script for now, just shows the files in the input directory
-md5sum $0
-INPUT_PATH=$(dirname $(readlink -f $0))
+echo "Preparing Script"
+echo "Input: $0"
+echo "Output Folder: $1"
 
 echo "Setting up STM32Cube environment..."
-echo "Displaying contents of the input directory: $INPUT_PATH"
-ls -l $INPUT_PATH
-tree $INPUT_PATH
+echo "Displaying contents of the input directory: $0"
+ls -lh $0
+tree $0
+
 
 # echo "Extracting script from input directory..."
 # echo "Extracting STM32Cube files..."
